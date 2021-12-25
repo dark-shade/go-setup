ad98629865fd5d41c20de0e28169af8074f96c8c
Sankul <sr282828@gmail.com>
2021-12-25 02:35:47 -0600

    updated description

--- cmd/root.go
+++ cmd/root.go
@@ -34,13 +34,9 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "go-setup",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "A CLI app for setting up golang projects",
+	Long: `A CLI app that provides the ability to setup and modify structure of multiple types of golang projects.
+It losely follows https://github.com/golang-standards/project-layout`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
